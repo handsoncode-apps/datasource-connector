4c69d9f78d17e5d8250d547c4262352f12634d81
Katarzyna Sandej <sandejka@gmail.com>
2018-04-23 10:21:04 +0200

    delete yaml2md.sh file

--- doc/yaml2md.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-command -v swagger-markdown >/dev/null 2>&1 || { echo >&2 "I require swagger-markdown but it's not installed.  Please install it globally then execute this script again"; exit 1; }
-swagger-markdown -i HOTswagger.yaml
-
